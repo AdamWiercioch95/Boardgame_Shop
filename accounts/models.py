fe82fad41173e8e13757170ff4a8ff1ffda3ca1f
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-06-11 14:36:47 +0200

    add accounts app with CustomUser

---
+++ accounts/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .validattors import validate_phone_number
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[validate_phone_number],
+        verbose_name='Phone number'
+    )
+    address = models.ForeignKey('Address', on_delete=models.CASCADE, verbose_name='Address')
+
+    class Meta:
+        verbose_name = 'User'
+
+    def __str__(self):
+        return self.username
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=255, verbose_name='City')
+    street = models.CharField(max_length=255, verbose_name='Street')
+    street_number = models.CharField(max_length=20, verbose_name='Street number')
+    house_number = models.CharField(max_length=20, verbose_name='House number')
+
+    class Meta:
+        verbose_name = 'Address'
+        verbose_name_plural = 'Addresses'
+        ordering = ['city', 'street', 'street_number', 'house_number']
+
+    def __str__(self):
+        return f"{self.city}, {self.street} {self.street_number}/{self.house_number}"
