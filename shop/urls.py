56096d58c4c14e2ebdcdf110893e4e571ec53a5d
Adam Wiercioch <adamwiercioch95@gmail.com>
2024-06-13 14:29:31 +0200

    add CRUD for Boardgame model

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,9 @@ from shop import views
 
 
 urlpatterns = [
-
+    path('boardgames_list/', views.BoardgameListView.as_view(), name='boardgames_list'),
+    path('boardgame_details/<int:pk>', views.BoardgameDetailView.as_view(), name='boardgame_details'),
+    path('boardgame_add/', views.BoardgameAddView.as_view(), name='boardgame_add'),
+    path('boardgame_update/<int:pk>', views.BoardgameUpdateView.as_view(), name='boardgame_update'),
+    path('boardgame_delete/<int:pk>', views.BoardgameDeleteView.as_view(), name='boardgame_delete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from shop.models import Boardgame
+
+
+class BoardgameListView(ListView):
+    model = Boardgame
+    template_name = "shop/boardgames_list.html"
+
+
+class BoardgameDetailView(DetailView):
+    model = Boardgame
+    template_name = "shop/boardgame_details.html"
+
+
+class BoardgameAddView(CreateView):
+    model = Boardgame
+    fields = '__all__'
+    template_name = "shop/form.html"
+    success_url = reverse_lazy('boardgames_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Add Boardgame'
+        return context
+
+
+class BoardgameUpdateView(UpdateView):
+    model = Boardgame
+    fields = '__all__'
+    template_name = "shop/form.html"
+    success_url = reverse_lazy('boardgames_list')
+
+    # def get_success_url(self):
+    #     return reverse('boardgame_update', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edit Boardgame'
+        return context
+
+
+class BoardgameDeleteView(DeleteView):
+    model = Boardgame
+    template_name = 'shop/boardgame_confirm_delete.html'
+    success_url = reverse_lazy('boardgames_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete Boardgame'
+        return context
+
